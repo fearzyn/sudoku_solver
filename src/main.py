65527af9982c3c0f6298af3b0e6ac8d9fc1beaaf
ramirezfernando <ramirez.fernando2003@gmail.com>
2024-04-18 12:53:30 -0500

    feat(main): add instructions and error handling

--- src/main.py
+++ src/main.py
@@ -2,8 +2,14 @@ from solver.solver import Solver
 import time
 
 board = []
+print("Enter 0 for empty cells. Be prepared to click the top left corner of the board.")
 for num in range(1, 10):
-    row = input("Enter row {}: ".format(num))
+    while True:
+        row = input("Enter row {}: ".format(num))
+        if len(row) == 9:
+            break
+        print("Invalid row. Please try again.")
+
     temp = []
     for num in row:
         temp.append(int(num))
@@ -11,5 +17,16 @@ for num in range(1, 10):
 
 sol = Solver()
 solved = sol.solve(board)
-time.sleep(3)
+
+print("Preparing to solve in 3 seconds. Click the top left corner of the board.")
+time.sleep(1)
+print("3")
+
+time.sleep(1)
+print("2")
+
+time.sleep(1)
+print("1")
+
 sol.automate(solved)
+print("Solved!")
